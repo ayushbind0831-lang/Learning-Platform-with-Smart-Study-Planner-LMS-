3c5ec68e79ef86d87abe59be7dade8bfdbb567d4
Ayush Bind <ayushbind0831@gmail.com>
2026-09-15 17:30:48 +0530

    backend says hello

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,16 @@
+from app import models
 from fastapi import FastAPI
+
+app = FastAPI(title="StudySmart API")
+
+@app.get("/")
+def read_root():
+    return {"message": "StudySmart backend is running"}
+from fastapi import FastAPI
+from app.database import engine, Base
+from app import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="StudySmart API")
 
